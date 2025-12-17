5854d0dc174a2ca00d25f440d287d7e8c6baafcf
Aakash Rathva <=>
2025-12-17 16:37:29 +0530

    Service added

---
+++ serveice-registery/src/main/java/com/example/serveice_registery/ServeiceRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.serveice_registery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeiceRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeiceRegisteryApplication.class, args);
+	}
+
+}
---
+++ serveice-registery/src/test/java/com/example/serveice_registery/ServeiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveice_registery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
